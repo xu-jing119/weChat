0acf3757e56daa264e70ad17b93de8a9701c978d
xujing <754130935@qq.com>
2020-02-09 18:49:14 +0800

    购物车模块开始

--- src/mixins/goods_detail/main.js
+++ src/mixins/goods_detail/main.js
@@ -2,7 +2,8 @@ import wepy from "wepy"
 export default class extends wepy.mixin{
     data={
         goods_id:'',
-        goodsInfo:[]
+        goodsInfo:[],
+        addressInfo:null
     }
     methods={
         getPreview(img){
@@ -10,6 +11,37 @@ export default class extends wepy.mixin{
                 current: img, // 当前显示图片的http链接
                 urls: this.goodsInfo.pics.map(x=>x.pics_big) // 需要预览的图片http链接列表
               })
+        },
+       async getAddress(){
+            const res = await wepy.chooseAddress().catch(err=>err)
+            if(res.errMsg!=='chooseAddress:ok'){
+                return wepy.baseToast('获取收货地址失败')
+            }
+            this.addressInfo = res
+            // console.log(res);
+            wepy.setStorageSync('address',res)
+            this.$apply()
+            
+        },
+        onAddCart(){
+            // console.log(this.goodsInfo);
+            // console.log(this.$parent.globalData);
+            this.$parent.addGoodsCart(this.goodsInfo)
+            wepy.showToast({
+                title:"加入购物车成功",
+                icon:"success"
+            })
+            
+        }
+    }
+    computed={
+        addressStr(){
+            if(this.addressInfo==null){
+                return '请选择收货地址'
+            }
+            const addr=this.addressInfo
+            const str = addr.provinceName+addr.cityName+addr.countyName+addr.detailInfo
+            return str
         }
     }
     onLoad(options){
