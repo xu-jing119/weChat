1f45fa4a8df75e1b3f71e8404578ceba418e5b9a
xujing <754130935@qq.com>
2020-02-10 21:49:14 +0800

    提交订单界面

--- src/mixins/order.js
+++ src/mixins/order.js
@@ -1,13 +1,30 @@
 import wepy from "wepy"
 export default class extends wepy.mixin{
     data={
-        addressInfo:''
+        addressInfo:null
+    }
+    onLoad(){
+        this.addressInfo= wepy.getStorageSync('address')||null
     }
     methods={
        async chooseAddr(){
             const res = await wepy.chooseAddress().catch(err=>err)
-            console.log(res);
-            
+            // console.log(res);
+            if(res.errMsg!=="chooseAddress:ok"){
+                return
+            }
+            this.addressInfo=res
+            // 将收货地址存储到本地
+            wepy.setStorageSync('address',res)
+            this.$apply()
+        }
+    }
+    computed={
+        isHaveAddr(){
+            if(this.addressInfo===null){
+                return false
+            }
+            return true
         }
     }
 }
